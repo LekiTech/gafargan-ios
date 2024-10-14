192cab0eeec0541df73ede75e04a3d2b335b49b4
Kamran Tadzjibov <ktadzjibov@gmail.com>
2024-10-14 09:11:30 +0200

    Initial Commit

---
+++ gafargan-ios/gafargan_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  gafargan_iosApp.swift
+//  gafargan-ios
+//
+//  Created by Kamran Tadzjibov on 14/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct gafargan_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
